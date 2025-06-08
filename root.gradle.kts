fa36fbf8326178808ada353346e7aeba9afb1bc6
MicrocontrollersDev <microcontrollersyt@gmail.com>
2025-06-08 13:25:46 -0700

    fix mappings

--- root.gradle.kts
+++ root.gradle.kts
@@ -10,20 +10,20 @@ preprocess {
     //     "1.8.9-forge"(10809, "srg")
     // }
 
-		"1.21.5-fabric"(12105, "srg") {
-			"1.21.5-neoforge"(12105, "yarn") {
+		"1.21.5-fabric"(12105, "yarn") {
+			"1.21.5-neoforge"(12105, "srg") {
 				"1.21.4-neoforge"(12104, "srg") {
 					"1.21.4-fabric"(12104, "yarn") {
-						"1.21.3-fabric"(12103, "srg") {
-							"1.21.3-neoforge"(12103, "yarn") {
+						"1.21.3-fabric"(12103, "yarn") {
+							"1.21.3-neoforge"(12103, "srg") {
 								"1.21.2-neoforge"(12102, "srg") {
 									"1.21.2-fabric"(12102, "yarn") {
-										"1.21.1-fabric"(12101, "srg") {
-											"1.21.1-neoforge"(12101, "yarn") {
+										"1.21.1-fabric"(12101, "yarn") {
+											"1.21.1-neoforge"(12101, "srg") {
 												"1.20.6-neoforge"(12006, "srg") {
 													"1.20.6-fabric"(12006, "yarn") {
-														"1.20.4-fabric"(12004, "srg") {
-															"1.20.4-forge"(12004, "yarn") {
+														"1.20.4-fabric"(12004, "yarn") {
+															"1.20.4-forge"(12004, "srg") {
 																"1.20.1-forge"(12001, "srg") {
 																	"1.20.1-fabric"(12001, "yarn") {
 																		"1.19.4-fabric"(11904, "yarn") {
