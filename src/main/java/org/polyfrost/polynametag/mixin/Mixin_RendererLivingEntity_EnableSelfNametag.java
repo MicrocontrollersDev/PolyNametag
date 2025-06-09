c79712f380b3c55df7454d9dcf6b6b2f0205a788
MicrocontrollersDev <microcontrollersyt@gmail.com>
2025-06-09 13:42:50 -0700

    revert not working mixin

--- src/main/java/org/polyfrost/polynametag/mixin/Mixin_RendererLivingEntity_EnableSelfNametag.java
+++ src/main/java/org/polyfrost/polynametag/mixin/Mixin_RendererLivingEntity_EnableSelfNametag.java
@@ -1,15 +1,13 @@
 package org.polyfrost.polynametag.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.Entity;
 import org.polyfrost.polynametag.PolyNametagConfig;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 //#if MC > 1.17.1
 //$$ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 //$$ import net.minecraft.client.MinecraftClient;
@@ -17,15 +15,16 @@ import org.spongepowered.asm.mixin.injection.At;
 //$$ import net.minecraft.entity.LivingEntity;
 //#endif
 
+@Debug(export = true)
 @Mixin(RendererLivingEntity.class)
 public class Mixin_RendererLivingEntity_EnableSelfNametag {
 
     //#if MC < 1.17.1
-    // TODO: nothing i do works wyvest pls help
-    @ModifyExpressionValue(method = "canRenderName(Lnet/minecraft/entity/EntityLivingBase;)Z", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/entity/RenderManager;livingPlayer:Lnet/minecraft/entity/Entity;"))
-    private Entity polynametag$enableSelfNametag(Entity original) {
+    // TODO: convert to a MixinExtras injection
+    @Redirect(method = "canRenderName(Lnet/minecraft/entity/EntityLivingBase;)Z", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/entity/RenderManager;livingPlayer:Lnet/minecraft/entity/Entity;"))
+    private Entity polynametag$enableSelfNametag(RenderManager instance) {
         if (!PolyNametagConfig.INSTANCE.getEnabled() || !PolyNametagConfig.INSTANCE.getShowOwnNametag()) {
-            return original;
+            return instance.livingPlayer;
         }
 
         return null;
