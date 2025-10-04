081aceb70b5d3f68a8652e5a4eaeffb7e07c212e
lowercasebtw <jejojora2@gmail.com>
2025-10-04 01:20:20 -0400

    PolyWeather :trol:

--- src/main/java/org/polyfrost/polynametag/mixin/client/Mixin_ReplaceTextRendering.java
+++ src/main/java/org/polyfrost/polynametag/mixin/client/Mixin_ReplaceTextRendering.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(Render.class)
 public abstract class Mixin_ReplaceTextRendering {
     @WrapOperation(method = "renderLivingLabel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawString(Ljava/lang/String;III)I"))
-    private int polyweather$replaceTextRendering(FontRenderer instance, String text, int x, int y, int color, Operation<Integer> original) {
+    private int polynametag$replaceTextRendering(FontRenderer instance, String text, int x, int y, int color, Operation<Integer> original) {
         if (PolyNametagConfig.isEnabled()) {
             return NametagRenderer.drawNametagString(OmniMatrixStacks.create(), text, x, y, new OmniColor(color));
         } else {
--- versions/1.12.2-fabric/src/main/java/org/polyfrost/polynametag/mixin/client/Mixin_ReplaceTextRendering.java
+++ versions/1.12.2-fabric/src/main/java/org/polyfrost/polynametag/mixin/client/Mixin_ReplaceTextRendering.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderer.class)
 public abstract class Mixin_ReplaceTextRendering<T extends Entity> {
     @Inject(method = "renderLabelIfPresent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;method_13427(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;FFFIFFZZ)V"))
-    private void polyweather$replaceTextRendering(T entity, String text, double x, double y, double z, int maxDistance, CallbackInfo ci) {
+    private void polynametag$replaceTextRendering(T entity, String text, double x, double y, double z, int maxDistance, CallbackInfo ci) {
         if (PolyNametagConfig.isEnabled()) {
             OmniColor color = OmniColors.WHITE;
             if (entity.isSneaking()) {
